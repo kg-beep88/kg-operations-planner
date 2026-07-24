c06a92494a60ea336e82b2b550e9f07c755645cf
kg-beep88 <kgchesterlee@gmail.com>
2026-07-24 10:55:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 window.KG_CONFIG = {
   supabaseUrl: 'https://cfbnotlhpfviigeveloz.supabase.co',
   supabasePublishableKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmYm5vdGxocGZ2aWlnZXZlbG96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE3MzIyOTIsImV4cCI6MjA5NzMwODI5Mn0.iuDHvrizHYFteNP5dEn1VyeZpRnhkDMGXboZSyT9mXw',
-}
+
+  googleClientId: "959288846598-vaaop77e1lkkrobu7podgahm0kttjnid.apps.googleusercontent.com",
+  googleCalendarId: "c2392b12e0bc46fd8b071a386ae243026985912bec4b3aaa8baef7c69c891de7@group.calendar.google.com"
+
+};
